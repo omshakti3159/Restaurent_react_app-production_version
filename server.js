2d9ff6cb170991668e27df8889a6cd73445cc910
omshakti3159 <mauryaomshakti@gmail.com>
2020-11-03 22:07:25 +0530

    env port added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const http = require('http')
 const path = require('path')
 const app = express()
-const Port = 3159
+const Port = process.env.PORT || 3159
 
 
 app.use(express.static(path.join(__dirname, 'build')))
