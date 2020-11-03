b79502fd6384f98851e85f69f03b88beb0065a54
omshakti3159 <mauryaomshakti@gmail.com>
2020-11-03 21:29:12 +0530

    build folder added and sever created to serve it

---
+++ build/precache-manifest.55f18a80d868ae15805322bfe959904d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f010f9bb9dcb10c92661450e82d2f31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73add83570d142110280",
+    "url": "/static/css/main.d3e09c52.chunk.css"
+  },
+  {
+    "revision": "4c222f57c55e36036ded",
+    "url": "/static/js/2.c0262b07.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c0262b07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73add83570d142110280",
+    "url": "/static/js/main.70cd88ee.chunk.js"
+  },
+  {
+    "revision": "ce0563f9c435ee02fdb1",
+    "url": "/static/js/runtime-main.396442b5.js"
+  }
+]);
--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 const express = require('express')
+const http = require('http')
 const path = require('path')
 const app = express()
-const Port = env.process.PORT || 3159
+const Port = 3159
+
+
+app.use(express.static(path.join(__dirname, 'build')))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build/index.html'))
+})
+app.listen(Port, (err) => {
+    if (err) throw err
+    console.log(`server is running on port${Port}`)
+})
